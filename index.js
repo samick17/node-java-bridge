a342237a48ca9aa0c17df350fca227064eacfc7a
samick17 <boneache@gmail.com>
2018-06-21 01:09:04 +0800

    1. remove static/instance, export directly

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ let jarPath = './Main.jar';
 let jarBridge = require('./lib/jar-bridge');
 
 jarBridge.load(jarPath)
-.then((data) => {
+.then((api) => {
+	console.log(api);
 	console.log('---- Instance ----');
-	console.log(data.instance.Base);
-	let b1 = new data.instance.Base();
+	console.log(api.Base);
+	let b1 = new api.Base();
 	console.log(b1.call('my arg'));
 
 	console.log('---- End of Instance ----');
 
 	console.log('---- Static Methods ----');
 
-	console.log(data.static.Main.dumpMethods('com.samick.jarbridge.instance.Concrete'));
+	console.log(api.Main.dumpMethods('com.samick.jarbridge.instance.Concrete'));
 	console.log('---- End of Static Methods ----');
 }, (err) => {
 	console.log(err);
